d68531241ede15f39a92e38897217abd1df4f88c
chip-davis <chipydavis99@gmail.com>
2024-06-26 13:40:40 -0500

    clean: cleanup

---
+++ Swift Shift/src/Manager/SpaceManager.swift
@@ -0,0 +1,8 @@
+//
+//  SpaceManager.swift
+//  Swift Shift
+//
+//  Created by Chip Davis on 6/25/24.
+//
+
+import Foundation
---
+++ Swift Shift/src/View/SpaceView.swift
@@ -0,0 +1,18 @@
+//
+//  SpaceView.swift
+//  Swift Shift
+//
+//  Created by Chip Davis on 6/25/24.
+//
+
+import SwiftUI
+
+struct SpaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpaceView()
+}
